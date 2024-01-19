1b0787e6139b9cbf08ccb01927f083fdda92eb91
Mariem Azzouz <mariemazzouz1234@gmail.com>
2024-01-19 17:15:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import APIComponent from './APIComponent';
+function App() {
+  return (
+    <div className="App">
+     <APIComponent />
+    </div>
+  );
+}
+
+export default App;
